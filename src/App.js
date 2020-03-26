72e84d8894ff97609096688c7c153245df769525
Tom Douglas <tomdoug@gmail.com>
2020-03-26 09:51:38 -0500

    Final changes

--- src/App.js
+++ src/App.js
@@ -24,9 +24,10 @@ function App() {
   return (
     <div className="container">
       <div className="header">As of this moment, the death rate of confirmed<span className="red">*</span> COVID-19 cases in the United States is:</div>
-      <div className="pct">{state.deaths > 1 ? (state.deaths / state.cases).toFixed(4) * 100 + "%": "Loading..."}</div>
+      <div className="pct">{state.deaths > 1 ? ((state.deaths / state.cases) * 100).toFixed(2) + "%": "Loading..."}</div>
+      <div className="stats">Confirmed Cases: {state.deaths > 1 ? state.cases.toLocaleString() : "Loading..."} | Confirmed Deaths: {state.deaths > 1 ? state.deaths.toLocaleString() : "Loading..."}</div>
       <div className="footer">
-        <span className="red">*</span> NOTE: confirmed cases are NOT all actual cases.  <br /><br />Hundreds of thousands or even millions of people have been infected with the virus.  You are -far- more likely to receive a test if you are symptomatic or have a severe case.
+        <span className="red">*</span> NOTE: confirmed cases are NOT all actual cases.  <br /><br />Hundreds of thousands, perhaps millions, of people have been infected with the virus and are asymptomatic or contracted the virus without being tested.
       </div>
     </div>
   )
